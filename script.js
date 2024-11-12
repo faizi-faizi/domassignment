0c7e534a0b71ec3181b5a9d69961f1b46ead2ca7
faaizmohammed <faaizbymohammed@gmail.com>
2024-11-12 10:59:42 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let element = document.getElementById("main-title")
+console.log(element)
+
+let elementContainer = document.getElementsByClassName("container")
+console.log(elementContainer)
+
+let elementPara = document.getElementsByTagName("p")
+console.log(elementPara)
+
+let elementButton = document.querySelector("button")
+console.log(elementButton)
+
+let elementButtonAll = document.querySelectorAll("button")
+console.log(elementButtonAll)
+
+let allPara = elementContainer[0].querySelectorAll("p");  
+console.log(allPara);
+
